8e90400648af15db3ae881905b9644aff3d09c3b
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2021-09-15 01:13:49 -0300

    Adiciona um pouco dos outros exercicios

--- Lab/Exercicios/atividade-aula-05/lista.py
+++ Lab/Exercicios/atividade-aula-05/lista.py
@@ -1,5 +1,9 @@
 import random
 import os
+import numpy as np
+
+
+
 
 class Methods():
     def selectionSort(aList):
@@ -56,12 +60,58 @@ def firstExercise(fileDir):
         print(f"Arquivo ({count}) >> Valor da mediana: {median}")
         print("...")
 def secondExercise(fileDir):
+    # A NASA finalmente anunciou a descoberta de vida fora da Terra. Uma sonda enviada à Marte detectou diversas
+    # formas de vida, e coletou indivíduos, que foram trazidos de volta a nosso planeta.
+    # Internamente, esses indivíduos alienígenas são bem diferentes das formas de vida terrestres. Enquanto que, por
+    # aqui, a ordem das bases no DNA determina as características do indivíduo, nos marcianos só importa quantas cópias
+    # de cada base estão presentes.
+    # Mais especificamente, o “mDNA” (ou DNA marciano) é composto de 15 bases, representadas pelas letras de A a O.
+    # Uma espécie é unicamente determinada pela quantidade de cada uma dessas bases que aparece em seu mDNA. Dois
+    # indivíduos de uma mesma espécie podem ter sequências de mDNA distintas. Por exemplo, um indivíduo com
+    # mDNA AABABJJ é da mesma espécie que um indivíduo com mDNA AJBAJBA, pois ambos possuem três bases A,
+    # duas bases B e duas bases J. Indivíduos ABACA e ABABCA, porém são de espécies diferentes: enquanto o primeiro
+    # possui apenas uma base B, o segundo possui duas cópias dessa base.
+    # São dadas as sequências de mDNA de n indivíduos. Determine quantas espécies distintas estão presentes.
+    # Entrada:
+    #   A entrada possui vários casos de teste. Cada caso de teste começa com um inteiro n, que representa o número de
+    #   indivíduos (0 < n < 65536). Em seguida há n linhas, cada uma das quais com a descrição do mDNA de um indivíduo.
+    #   Essa descrição é composta apenas por letras maiúsculas entre A e O (inclusive), e possui no máximo 100 caracteres.
+    #   A entrada termina com n=0, que não deve ser processado.
     if not os.path.exists(fileDir): os.makedirs(fileDir)
+    def generateInput(path):
+        possibleLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O"]
+        file = open(path, "w+", encoding="UTF-8")
+        nChoice = np.arange(0, 65536)
+        for i in nChoice:
+            file.write(str(i) + "\n")
+            nChar = random.randint(0, 100)
+            for j in range(i):
+                output = ""
+                for i in range(nChar):
+                    nList = random.randint(0, len(possibleLetters)-1)
+                    output += possibleLetters[nList]
+                file.write(output)
+                file.write("\n")
+        file.close()
+    generateInput(f"{fileDir}/alien.txt")
 def thirdExercise(fileDir):
+    # vai ser realizada a sensacional final mundial da fórmula 1. Os competidores se alinham na largada e
+    # disputam a corrida. Você vai ter acesso aos grids de largada e de chegada. A questão é determinar o número
+    # mínimo de ultrapassagens que foram efetuadas durante a competição.
+    # Entrada
+    # Cada caso de teste utiliza três linhas. A primeira linha de um caso de teste contém um inteiro N (2 ≤ N ≤ 24)
+    # indicando o número de competidores. Cada competidor é identificado com um número de 1 a N. A segunda
+    # linha de cada caso tem os N competidores, em ordem do grid de largada. A terceira linha de cada caso tem
+    # os mesmos competidores, porém agora na ordem de chegada.
+    # Saída
+    # Para cada caso de teste imprima uma linha contendo um único número inteiro, que indica o número mínimo
+    # de ultrapassagens necessárias para se chegar do grid de largada ao grid de chegada
     if not os.path.exists(fileDir): os.makedirs(fileDir)
+    
 
 
-def main(showFirst = True, showSecond = True, showThird = True):
+# Para habilitar o exercício, só alterar o valor do parâmetor do mesmo para True
+def main(showFirst = False, showSecond = False, showThird = False):
     # 1-)
     if showFirst:
         print("Primeiro exercício:\n")
@@ -74,4 +124,4 @@ def main(showFirst = True, showSecond = True, showThird = True):
     if showThird:
         print("Terceiro exercício: ")
         thirdExercise("third_input")
-main(False, True, True)
+main(showFirst = False, showSecond = True, showThird = True)
