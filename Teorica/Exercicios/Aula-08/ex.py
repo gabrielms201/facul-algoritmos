f1cce1d4d32d5446be29506a89136129e5d8209b
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2021-10-13 16:08:53 -0300

    Termina ex1

---
+++ Teorica/Exercicios/Aula-08/ex.py
@@ -0,0 +1,15 @@
+def ex1():
+    # 1) Na busca linear implementada em aula, o algoritmo tentava encontrar a primeira ocorrência do elemento procurado. Queremos agora encontrar a última ocorrência (caso exista) de um determinado elemento. Para isto, desenvolva a função:
+    #       def linearSearchLast(L, e):
+    # que devolva, caso exista, o último índice ocupado pelo elemento e na lista L. Caso o elemento não exista na lista, a função deverá retornar -1
+    def linearSearchLast(L, e):
+        find = False
+        for i in range(len(L)):
+            if L[i] == e: 
+                find = True
+                lastIndex = i
+        if find: return lastIndex
+        return -1
+    lista = [1,2,3,4,4,6]
+    print(linearSearchLast(lista, 4))
+ex1()
